4ba1e34b637b9dc386351f3b4663b34b06565900
HellRaiser <akshat@nineleaps.com>
2019-02-06 11:08:01 +0530

    Added url's and views function

--- blogClone/mysite/blog/views.py
+++ blogClone/mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from django.view.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from blog.models import Post,comments
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -46,3 +47,40 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+#######################################################################
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm
+    return render(request,'blog/comment_form.html',{'form':form})
+#####
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
--- blogClone/mysite/mysite/urls.py
+++ blogClone/mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path,include
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path(r'accounts/login/$',views.login,name='login'),
+    path(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
 ]
